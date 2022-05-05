e7bc58c9381f4b231505dcdd656f7c5afafc19f7
Riari <3583774+Riari@users.noreply.github.com>
2022-05-05 19:53:10 +0100

    Update Quad and add cooldowns to pickups

--- scripts/Pickup.gd
+++ scripts/Pickup.gd
@@ -1,25 +1,54 @@
 extends Area
 
-var origin
-
 export(String, "AMMO_BLASTER") var pickup_type = "AMMO_BLASTER"
 export var pickup_amount = 10
+export var cooldown_time = 5
+export var cooldown_color = Color(0.5, 0.5, 0.5, 0.5)
 
-onready var type_nodes = {
+onready var mesh_containers = {
 	"AMMO_BLASTER": $Types/BlasterAmmo
 }
+
+var origin
+var active = true
+var cooldown_timer = cooldown_time
+var mesh_container: Node
+var meshes = []
 
 signal picked_up
 
 func _ready():
 	origin = self.global_transform.origin
-	type_nodes[pickup_type].show()
+	mesh_container = mesh_containers[pickup_type]
+	mesh_container.show()
+
+	for c in mesh_container.get_children():
+		if c is MeshInstance:
+			meshes.append(c)
 
 func _process(delta):
 	self.rotate_y(delta)
 	self.global_transform.origin.y = origin.y + (cos(OS.get_ticks_msec() / 300.0) / 10.0)
 
+	if !active:
+		if cooldown_timer <= 0:
+			set_active(true)
+			cooldown_timer = cooldown_time
+		else:
+			cooldown_timer -= delta
+
+func set_active(is_active: bool):
+	active = is_active
+	for mesh in meshes:
+		if is_active:
+			mesh.material_override = null
+		else:
+			var material = SpatialMaterial.new()
+			material.flags_transparent = true
+			material.albedo_color = cooldown_color
+			mesh.material_override = material
+
 func on_body_entered(node: Node):
-	if node.get_meta("type") == "player":
+	if active && node.get_meta("type") == "player":
+		set_active(false)
 		emit_signal("picked_up", node, pickup_type, pickup_amount)
-		queue_free()
